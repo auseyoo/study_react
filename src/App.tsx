274f885408b34dbe7123a963baef0c76e20c09ef
윤미연(X1294660) <auseyoo@cnspartner.com>
2021-10-09 18:38:00 +0900

    계산기 퍼블리싱

--- src/App.tsx
+++ src/App.tsx
@@ -15,10 +15,38 @@ function App() {
   };
 
   return (
-    <div>
-      {abc}
-      <button onClick={handleButtonClick}>미연아</button>
-      <button onClick={handleButtonClick2}>잘잤냐</button>
+    <div className="row-wrapper">
+      <div className="name">
+        <span>계산기1</span>
+      </div>
+      <input className="display" />
+      <div className="row">
+        <button>9</button>
+        <button>8</button>
+        <button>7</button>
+        <button>*</button>
+      </div>
+      <div className="row">
+        <button>6</button>
+        <button>5</button>
+        <button>4</button>
+        <button>+</button>
+      </div>
+      <div className="row">
+        <button>3</button>
+        <button>2</button>
+        <button>1</button>
+        <button>-</button>
+      </div>
+      <div className="row">
+        <button>0</button>
+        <button>.</button>
+        <button>=</button>
+        <button>/</button>
+      </div>
+      <div>
+        <button className="clear-button">Clear</button>
+      </div>
     </div>
   );
 }
