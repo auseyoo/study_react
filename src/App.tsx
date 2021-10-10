5b3f8a94b27ada28d51afa95de998cdee209365e
윤미연(X1294660) <auseyoo@cnspartner.com>
2021-10-10 21:06:38 +0900

    ddd

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button, TextField } from '@mui/material';
 
 function App() {
   const [abc, setAbc] = useState('화이팅');
@@ -12,105 +13,6 @@ function App() {
   const [secondNumber, setSecondNumber] = useState(0);
   const [resultNumber, setResultNumber] = useState(0);
 
-  const handleButtonClick = () => {
-    console.log('click');
-    setAbc('잘자');
-  };
-
-  const handleButtonClick2 = () => {
-    setAbc('잘잤다');
-  };
-
-  const numberButtonClick = () => {
-    console.log('0');
-
-    if (operatorString === '') {
-      setFirstString(firstString + '0');
-    } else {
-      setSecondString(secondString + '0');
-    }
-  };
-  const numberButtonClick1 = () => {
-    console.log('1');
-
-    if (operatorString === '') {
-      setFirstString(firstString + '1');
-    } else {
-      setSecondString(secondString + '1');
-    }
-  };
-  const numberButtonClick2 = () => {
-    console.log('2');
-
-    if (operatorString === '') {
-      setFirstString(firstString + '2');
-    } else {
-      setSecondString(secondString + '2');
-    }
-  };
-  const numberButtonClick3 = () => {
-    console.log('3');
-
-    if (operatorString === '') {
-      setFirstString(firstString + '3');
-    } else {
-      setSecondString(secondString + '3');
-    }
-  };
-  const numberButtonClick4 = () => {
-    console.log('4');
-
-    if (operatorString === '') {
-      setFirstString(firstString + '4');
-    } else {
-      setSecondString(secondString + '4');
-    }
-  };
-  const numberButtonClick5 = () => {
-    console.log('5');
-
-    if (operatorString === '') {
-      setFirstString(firstString + '5');
-    } else {
-      setSecondString(secondString + '5');
-    }
-  };
-  const numberButtonClick6 = () => {
-    console.log('6');
-
-    if (operatorString === '') {
-      setFirstString(firstString + '6');
-    } else {
-      setSecondString(secondString + '6');
-    }
-  };
-  const numberButtonClick7 = () => {
-    console.log('7');
-
-    if (operatorString === '') {
-      setFirstString(firstString + '7');
-    } else {
-      setSecondString(secondString + '7');
-    }
-  };
-  const numberButtonClick8 = () => {
-    console.log('8');
-
-    if (operatorString === '') {
-      setFirstString(firstString + '8');
-    } else {
-      setSecondString(secondString + '8');
-    }
-  };
-  const numberButtonClick9 = () => {
-    console.log('9');
-
-    if (operatorString === '') {
-      setFirstString(firstString + '9');
-    } else {
-      setSecondString(secondString + '9');
-    }
-  };
   const numberButtonClickMinus = () => {
     console.log('-');
     setOperatorString('-');
@@ -154,6 +56,203 @@ function App() {
     setResultNumber(0);
   };
 
+  const buttonLoop = () => {
+    const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    let result: any = [];
+    arr.map((miyeon, inx) => {
+      console.log('inx', inx);
+      result.push(
+        <Button
+          onClick={() => {
+            rain(String(miyeon));
+          }}
+        >
+          {miyeon}
+        </Button>,
+      );
+      if (inx % 3 === 2) {
+        result.push(<br />);
+      }
+    });
+    return result;
+    <Button
+      onClick={() => {
+        rain('9');
+      }}
+    >
+      9
+    </Button>;
+  };
+
+  const rain = (param: string) => {
+    // for (let inx = 0; inx <= 5; inx++) {
+    //   console.log(inx);
+    // }
+    if (operatorString === '') {
+      setFirstString(firstString + param);
+    } else {
+      setSecondString(secondString + param);
+    }
+
+    const arr = [1, 2, 3, 4];
+    arr.map((miyeon) => {
+      // console.log('hrere');
+      // console.log(miyeon);
+    });
+    arr.forEach((miyeon) => {
+      // console.log('hrere2');
+      // console.log(miyeon);
+    });
+
+    const ManagementRowData3 = [
+      {
+        System: '법인카드',
+        Process: '한도증액',
+        'Submission No': 'S2021072100001',
+        'Submission Title': '법인카드 한도증액 3만불 이상 CFO승인 필수',
+        Registrant: '김바다 bada2 kim',
+        RegisterDept: '자금팀 123456',
+        'Register Date': '2021-6-22',
+        'Standard Authorization Matrix': '-',
+        Status: '',
+        'Due Date': '',
+        'Target Cnt.': '84',
+        'Confirm Cnt.': '80',
+        'Hold Cnt.': 0,
+        'Close Date': '',
+      },
+      {
+        System: '법인카드',
+        Process: '한도증액',
+        'Submission No': 'S2021072100001',
+        'Submission Title': '법인카드 한도증액 3만불 이상 CFO승인 필수',
+        Registrant: '김바다 bada2 kim',
+        RegisterDept: '자금팀 123456',
+        'Register Date': '2021-6-22',
+        'Standard Authorization Matrix': '-',
+        Status: '',
+        'Due Date': '',
+        'Target Cnt.': '84',
+        'Confirm Cnt.': '80',
+        'Hold Cnt.': 0,
+        'Close Date': '',
+      },
+      {
+        System: '',
+        Process: '한도증액',
+        'Submission No': 'S2021072100001',
+        'Submission Title': '법인카드 한도증액 3만불 이상 CFO승인 필수',
+        Registrant: '김바다 bada2 kim',
+        RegisterDept: '자금팀 123456',
+        'Register Date': '2021-6-22',
+        'Standard Authorization Matrix': '-',
+        Status: '',
+        'Due Date': '',
+        'Target Cnt.': '84',
+        'Confirm Cnt.': '80',
+        'Hold Cnt.': 0,
+        'Close Date': '',
+      },
+      {
+        System: '',
+        Process: '한도증액',
+        'Submission No': 'S2021072100001',
+        'Submission Title': '법인카드 한도증액 3만불 이상 CFO승인 필수',
+        Registrant: '김바다 bada2 kim',
+        RegisterDept: '자금팀 123456',
+        'Register Date': '2021-6-22',
+        'Standard Authorization Matrix': '-',
+        Status: '',
+        'Due Date': '',
+        'Target Cnt.': '84',
+        'Confirm Cnt.': '80',
+        'Hold Cnt.': 0,
+        'Close Date': '',
+      },
+      {
+        System: '',
+        Process: '한도증액',
+        'Submission No': 'S2021072100001',
+        'Submission Title': '법인카드 한도증액 3만불 이상 CFO승인 필수',
+        Registrant: '김바다 bada2 kim',
+        RegisterDept: '자금팀 123456',
+        'Register Date': '2021-6-22',
+        'Standard Authorization Matrix': '-',
+        Status: '',
+        'Due Date': '',
+        'Target Cnt.': '84',
+        'Confirm Cnt.': '80',
+        'Hold Cnt.': 0,
+        'Close Date': '',
+      },
+      {
+        System: '',
+        Process: '한도증액',
+        'Submission No': 'S2021072100001',
+        'Submission Title': '법인카드 한도증액 3만불 이상 CFO승인 필수',
+        Registrant: '김바다 bada2 kim',
+        RegisterDept: '자금팀 123456',
+        'Register Date': '2021-6-22',
+        'Standard Authorization Matrix': '-',
+        Status: '',
+        'Due Date': '',
+        'Target Cnt.': '84',
+        'Confirm Cnt.': '80',
+        'Hold Cnt.': 0,
+        'Close Date': '',
+      },
+    ];
+    const MonitoringRowData2 = [
+      {
+        No: '1',
+        Subsidiary: 'LGEKR',
+        'Person in charge': '김시내 sinae3.kim',
+        Status: 'Confirmed',
+        Comment: 'CFO Role 추가 및 기존 1개 전결을 금액으로 분리하여 1개 전결을 추가완료',
+        'Confirm Date': '2021-07-22',
+        Hold: 'N',
+        'Hold Date': '',
+      },
+      {
+        No: '2',
+        Subsidiary: 'LGECH',
+        'Person in charge': '장**',
+        Status: 'Pending',
+        Comment: '',
+        'Confirm Date': '',
+        Hold: '',
+        'Hold Date': '',
+      },
+      {
+        No: '3',
+        Subsidiary: 'LGEUS',
+        'Person in charge': 'James**',
+        Status: 'Rejected',
+        Comment: 'US 법인 예외 처리 요청',
+        'Confirm Date': '2021-07-22',
+        Hold: '',
+        'Hold Date': '',
+      },
+      {
+        No: '4',
+        Subsidiary: 'LGEKR',
+        'Person in charge': '김시내 sinae3.kim',
+        Status: 'Confirmed',
+        Comment: 'CFO Role 추가 및 기존 1개 전결을 금액으로 분리하여 1개 전결을 추가완료',
+        'Confirm Date': '2021-07-22',
+        Hold: 'N',
+        'Hold Date': '',
+      },
+    ];
+    MonitoringRowData2.map((miyeon, index) => {
+      console.log('index:', index);
+      console.log(miyeon.No);
+    });
+    // let miyeon;
+    // console.log('***********************************');
+    // console.log(miyeon);
+  };
+
   return (
     <div className="row-wrapper">
       <div className="name">
@@ -170,35 +269,95 @@ function App() {
       <br />
       secondNumber: {secondNumber}
       <br />
-      resultNumber: {resultNumber}
+      <TextField id="outlined-basic" value={resultNumber} label="resultNumber" variant="outlined" />
+      <br />
+      {buttonLoop()}
       <div className="row">
-        <button onClick={numberButtonClick9}>9</button>
-        <button onClick={numberButtonClick8}>8</button>
-        <button onClick={numberButtonClick7}>7</button>
-        <button>*</button>
+        <Button
+          onClick={() => {
+            rain('9');
+          }}
+        >
+          9
+        </Button>
+        <Button
+          onClick={() => {
+            rain('8');
+          }}
+        >
+          8
+        </Button>
+        <Button
+          onClick={() => {
+            rain('7');
+          }}
+        >
+          7
+        </Button>
+        <Button>*</Button>
       </div>
       <div className="row">
-        <button onClick={numberButtonClick6}>6</button>
-        <button onClick={numberButtonClick5}>5</button>
-        <button onClick={numberButtonClick4}>4</button>
-        <button onClick={numberButtonClickPlus}>+</button>
+        <Button
+          onClick={() => {
+            rain('6');
+          }}
+        >
+          6
+        </Button>
+        <Button
+          onClick={() => {
+            rain('5');
+          }}
+        >
+          5
+        </Button>
+        <Button
+          onClick={() => {
+            rain('4');
+          }}
+        >
+          4
+        </Button>
+        <Button onClick={numberButtonClickPlus}>+</Button>
       </div>
       <div className="row">
-        <button onClick={numberButtonClick3}>3</button>
-        <button onClick={numberButtonClick2}>2</button>
-        <button onClick={numberButtonClick1}>1</button>
-        <button onClick={numberButtonClickMinus}>-</button>
+        <Button
+          onClick={() => {
+            rain('3');
+          }}
+        >
+          3
+        </Button>
+        <Button
+          onClick={() => {
+            rain('2');
+          }}
+        >
+          2
+        </Button>
+        <Button
+          onClick={() => {
+            rain('1');
+          }}
+        >
+          1
+        </Button>
+        <Button onClick={numberButtonClickMinus}>-</Button>
       </div>
       <div className="row">
-        <button onClick={numberButtonClick}>0</button>
-        <button onClick={numberButtonClickDot}>.</button>
-        <button onClick={numberButtonClickEqal}>=</button>
-        <button onClick={numberButtonClick1}>/</button>
+        <Button
+          onClick={() => {
+            rain('0');
+          }}
+        ></Button>
+        <Button onClick={numberButtonClickDot}>.</Button>
+        <Button onClick={numberButtonClickEqal}>=</Button>
+        <Button>/</Button>
       </div>
       <div>
-        <button className="clear-button" onClick={handleClear}>
+        <Button className="clear-button" onClick={handleClear}>
           Clear
-        </button>
+        </Button>
       </div>
     </div>
   );
